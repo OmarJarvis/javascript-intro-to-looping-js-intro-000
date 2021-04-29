ebb62cfd1c9d2a1dd8b6907b0ce4894ef819df09
Omar Jarvis <thejarvisgroupllc@gmail.com>
2021-04-29 01:52:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,14 @@
-function forLoop(array) {
+function forLoop(arr) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      console.log(`${array} I am 1 strange loop.`);
+
+
+      arr.push(`I am 1 strange loop.`);
     } else {
-      console.log(`${array} I am ${i} strange loops.`);
+      arr.push(`I am ${i} strange loops.`);
     }
   }
-}
 
-forLoop(array);
+  // Return your array
+  return arr;
+}
